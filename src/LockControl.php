4e4dc9b7dd97ddb77460c4f6a60dcc8efeccc278
chenjiahao <271375247@qq.com>
2020-02-01 15:01:51 +0800

    a

---
+++ src/LockControl.php
@@ -0,0 +1,72 @@
+<?php declare(strict_types=1);
+
+
+namespace Swoft\RedisLock;
+
+
+use function bean;
+use ReflectionException;
+use Swoft\Bean\Exception\ContainerException;
+use Swoft\SwoftComponent;
+use Swoft\Redis\Pool;
+use LockSpace\RedisLock;
+use LockSpace\RedisNewLock;
+
+/**
+ * Class LockControl
+ *
+ * @since 2.0
+ */
+class LockControl  
+{
+    
+    /**
+     *  
+     *
+     * @var Pool
+     */
+    private $redis;
+
+    /**
+     *  
+     *
+     * @var RedisLock
+     */
+    private $redisLock;
+
+    /**
+     *  
+     *
+     * @var RedisNewLock
+     */
+    private $redisNewLock;
+
+
+    /**
+     * LockControl constructor.
+     *
+     * @param Pool $redis
+     */
+    public function __construct(Pool $redis)
+    {
+        $this->redis = $redis;
+        $this->init();
+    }
+
+    private function init()
+    {
+        $this->redisLock = new RedisLock($this->redis);
+        $this->redisNewLock = new RedisNewLock($this->redis);
+    }
+
+    public function  RedisLock()
+    {
+        return  $this->redisLock;
+    }
+
+    public function  RedisNewLock()
+    {
+        return  $this->redisNewLock ;
+    }
+
+}
